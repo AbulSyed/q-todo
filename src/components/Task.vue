054031b416d3b70f66fcc306814f17b3d38fa395
SYED <abul.syed@oup.com>
2021-06-28 09:41:07 +0100

    getters

---
+++ src/components/Task.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-item
+    clickable
+    @click="task.completed = !task.completed"
+    :class="task.completed ? 'bg-green-3' : 'bg-grey-1'"
+    v-ripple
+  >
+    <q-item-section side top>
+      <q-checkbox v-model="task.completed" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label
+        :class="{ 'text-strike': task.completed }"
+      >
+        {{ task.name }}
+      </q-item-label>
+    </q-item-section>
+
+    <q-item-section side top>
+      <q-item-label caption>{{ task.dueDate }}</q-item-label>
+      <q-item-label caption>{{ task.dueTime }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  props: ['task', 'id']
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,41 +4,22 @@
       bordered
       separator
     >
-
-      <q-item
+      <Task
         v-for="(task, key) in tasks"
         :key="key"
-        clickable
-        @click="task.completed = !task.completed"
-        :class="task.completed ? 'bg-green-3' : 'bg-grey-1'"
-        v-ripple
-      >
-        <q-item-section side top>
-          <q-checkbox v-model="task.completed" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label
-            :class="{ 'text-strike': task.completed }"
-          >
-            {{ task.name }}
-          </q-item-label>
-        </q-item-section>
-
-        <q-item-section side top>
-          <q-item-label caption>{{ task.dueDate }}</q-item-label>
-          <q-item-label caption>{{ task.dueTime }}</q-item-label>
-        </q-item-section>
-      </q-item>
-
+        :task="task"
+        :id="key"
+      />
     </q-list>
   </q-page>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import Task from '../components/Task.vue'
 
 export default {
+  components: { Task },
   computed: {
     ...mapGetters('tasks', ['tasks'])
   }
